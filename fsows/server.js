530dbfd7759466f4f2ad2ec589c2aec3595d8aa7
Gilaxy04 <79372051+franck403@users.noreply.github.com>
2026-01-02 11:40:10 -0500

    Add SSL support to WebSocket server

--- fsows/server.js
+++ fsows/server.js
@@ -1,30 +1,33 @@
 const { WebSocketServer } = require('ws');
+const https = require('https');
 const fs = require('fs').promises;
+const fsSync = require('fs');
 
-function startServer(port) {
-    const wss = new WebSocketServer({ port });
+function startServer(port, sslOptions = null) {
+    let wss;
+    
+    if (sslOptions) {
+        const server = https.createServer(sslOptions);
+        wss = new WebSocketServer({ server });
+        server.listen(port);
+    } else {
+        wss = new WebSocketServer({ port });
+    }
 
     wss.on('connection', (ws) => {
         ws.on('message', async (data) => {
             const { id, method, args } = JSON.parse(data);
             try {
-                if (typeof fs[method] !== 'function') {
-                    throw new Error(`Method ${method} not found on fs.promises`);
-                }
-
                 let result = await fs[method](...args);
-                
                 if (Buffer.isBuffer(result)) {
                     result = { _isBuffer: true, data: result.toString('base64') };
                 }
-                
                 ws.send(JSON.stringify({ id, result }));
             } catch (error) {
                 ws.send(JSON.stringify({ id, error: error.message }));
             }
         });
     });
-
     return wss;
 }
 
